f27bfa389a73f58b57db34246296f4f4aba4dfcf
Rishi Singh <rs3949427@gmail.com>
2023-07-13 14:44:46 +0530

    added frontend

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const URL = "https://rishibookingbackend.onrender.com/api";
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(URL+url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(URL+url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
